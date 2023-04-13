1ee65859fb4fd3a2673f20c640d0a514196d506e
Kien22092001 <kiendoba1905@gmail.com>
2023-04-13 23:09:31 +0700

    fix config

--- src/main/java/com/se/kltn/vietstack/config/SimpleCORSFilter.java
+++ src/main/java/com/se/kltn/vietstack/config/SimpleCORSFilter.java
@@ -1,19 +1,28 @@
 package com.se.kltn.vietstack.config;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+
 @Component
-public class SimpleCORSFilter  implements WebMvcConfigurer {
+public class SimpleCORSFilter implements Filter {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("")
-                .allowCredentials(true);
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, sessioncookie");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(req, res);
     }
 
+    // other methods omitted for brevity
 }
