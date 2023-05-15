bcf12aee57b75b2c00f5258fb95979d2852c11df
Duy <heiligegoldia@gmail.com>
2023-05-15 18:44:20 +0700

    0.9.1

--- src/main/java/com/se/kltn/vietstack/controller/TagController.java
+++ src/main/java/com/se/kltn/vietstack/controller/TagController.java
@@ -1,6 +1,9 @@
 package com.se.kltn.vietstack.controller;
 
+import com.google.firebase.auth.FirebaseAuthException;
 import com.se.kltn.vietstack.model.tag.Tag;
+import com.se.kltn.vietstack.model.user.User;
+import com.se.kltn.vietstack.service.AccountService;
 import com.se.kltn.vietstack.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +20,9 @@ public class TagController {
     @Autowired
     private TagService tagService;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/getAllTag")
     public ResponseEntity<List<Tag>> getAllTag() throws ExecutionException, InterruptedException {
         List<Tag> tl = tagService.getAllTag();
@@ -32,6 +38,77 @@ public class TagController {
         else {
             return ResponseEntity.ok(t);
         }
+    }
+
+    @PostMapping("/addTag")
+    public ResponseEntity<String> addTag(@CookieValue("sessionCookie") String ck, @RequestBody Tag tag) throws FirebaseAuthException, ExecutionException, InterruptedException {
+        User user = accountService.verifySC(ck);
+        if(user.getUid()==null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorize failed");
+        }
+        else {
+            String role = accountService.getUserClaims(ck);
+            if (!role.equals("Admin")) {
+                return ResponseEntity.ok("Access denied");
+            }
+            else {
+                String s = tagService.addTag(tag);
+                return ResponseEntity.ok(s);
+            }
+        }
+    }
+
+    @PutMapping("/editTag/{tid}")
+    public ResponseEntity<String> editTag(@CookieValue("sessionCookie") String ck, @PathVariable("tid") String tid, @RequestBody Tag tag) throws FirebaseAuthException, ExecutionException, InterruptedException {
+        User user = accountService.verifySC(ck);
+        if(user.getUid()==null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorize failed");
+        }
+        else {
+            String role = accountService.getUserClaims(ck);
+            if (!role.equals("Admin")) {
+                return ResponseEntity.ok("Access denied");
+            }
+            else {
+                Tag t = tagService.getTagByName(tag.getName());
+                if(t.getTid()==null || t.getTid().equals(tid)) {
+                    tag.setTid(tid);
+                    String s = tagService.editTag(tag);
+                    return ResponseEntity.ok(s);
+                }
+                else {
+                    return ResponseEntity.ok("Tag already exist");
+                }
+            }
+        }
+    }
+
+    @DeleteMapping("/deleteTag/{tid}")
+    public ResponseEntity<String> deleteTag(@CookieValue("sessionCookie") String ck, @PathVariable("tid") String tid) throws FirebaseAuthException, ExecutionException, InterruptedException {
+        User user = accountService.verifySC(ck);
+        if(user.getUid()==null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorize failed");
+        }
+        else {
+            String role = accountService.getUserClaims(ck);
+            if (!role.equals("Admin")) {
+                return ResponseEntity.ok("Access denied");
+            } else {
+                if(tagService.checkTag(tid)) {
+                    String s =tagService.deleteTag(tid);
+                    return ResponseEntity.ok(s);
+                }
+                else {
+                    return ResponseEntity.ok("Tag is in use");
+                }
+            }
+        }
+    }
+
+    @GetMapping("/getTagByName")
+    public ResponseEntity<Tag> getTagByName(@RequestBody Tag tag) throws ExecutionException, InterruptedException {
+        Tag t = tagService.getTagByName(tag.getName());
+        return ResponseEntity.ok(t);
     }
 
 }
--- src/main/java/com/se/kltn/vietstack/service/TagService.java
+++ src/main/java/com/se/kltn/vietstack/service/TagService.java
@@ -7,6 +7,7 @@ import com.se.kltn.vietstack.model.tag.Tag;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -14,6 +15,57 @@ import java.util.concurrent.ExecutionException;
 public class TagService {
 
     Firestore db = FirestoreClient.getFirestore();
+
+    public String getLastTid() throws ExecutionException, InterruptedException {
+        CollectionReference ref = db.collection("Tag");
+        ApiFuture<QuerySnapshot> api = ref.get();
+        QuerySnapshot doc = api.get();
+        List<QueryDocumentSnapshot> docs = doc.getDocuments();
+        if(docs.size() == 0){
+            return "0";
+        }
+        else{
+            List<Integer> docId = new ArrayList<>();
+            for(QueryDocumentSnapshot ds : docs){
+                docId.add(Integer.parseInt(ds.getId()));
+            }
+            Collections.sort(docId);
+            return String.valueOf(docId.get(docId.size()-1));
+        }
+    }
+
+    public Tag getTagByName(String name) throws ExecutionException, InterruptedException {
+        CollectionReference ref = db.collection("Tag");
+        Query query = ref.whereEqualTo("name", name);
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+        List<QueryDocumentSnapshot> docs = querySnapshot.get().getDocuments();
+        for(QueryDocumentSnapshot ds : docs){
+            return ds.toObject(Tag.class);
+        }
+        return new Tag();
+    }
+
+    public String addTag(Tag tag) throws ExecutionException, InterruptedException {
+        Tag t = getTagByName(tag.getName());
+        if(t.getTid()==null){
+            int newTid = Integer.parseInt(getLastTid()) + 1;
+            String tid = String.valueOf(newTid);
+            tag.setTid(tid);
+
+            ApiFuture<WriteResult> api = db.collection("Tag").document(tag.getTid()).set(tag);
+            api.get();
+            return tag.getTid();
+        }
+        else {
+            return "Tag already exist";
+        }
+    }
+
+    public String editTag(Tag tag) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> api = db.collection("Tag").document(tag.getTid()).set(tag);
+        api.get();
+        return tag.getTid();
+    }
 
     public List<Tag> getAllTag() throws ExecutionException, InterruptedException {
         List<Tag> tl = new ArrayList<>();
@@ -42,6 +94,29 @@ public class TagService {
             return tag;
         }
         return new Tag();
+    }
+
+    public boolean checkTag(String tid) throws ExecutionException, InterruptedException {
+        CollectionReference ref = db.collection("QuestionTag");
+        Query query = ref.whereEqualTo("tid", tid);
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+        List<QueryDocumentSnapshot> docs = querySnapshot.get().getDocuments();
+        if(!docs.isEmpty())
+            return false;
+        else
+            return true;
+    }
+
+    public String deleteTag(String tid) {
+        try{
+            ApiFuture<WriteResult> writeResult = db.collection("Tag").document(tid).delete();
+            writeResult.get();
+            return "Tag deleted";
+        } catch (ExecutionException e) {
+            return "Tag not found";
+        } catch (InterruptedException e) {
+            return "Tag not found";
+        }
     }
 
 }
