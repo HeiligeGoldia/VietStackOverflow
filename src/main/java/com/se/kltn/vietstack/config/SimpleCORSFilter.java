7131073eea505a3d43c8e402dcb642a67f7ed01e
Duy <heiligegoldia@gmail.com>
2023-05-22 14:58:45 +0700

    fix config cors

--- src/main/java/com/se/kltn/vietstack/config/SimpleCORSFilter.java
+++ src/main/java/com/se/kltn/vietstack/config/SimpleCORSFilter.java
@@ -2,24 +2,44 @@ package com.se.kltn.vietstack.config;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 
-@Component
-public class SimpleCORSFilter implements Filter {
+//@Component
+@Configuration
+public class SimpleCORSFilter {
+//        implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//
+//        HttpServletResponse response = (HttpServletResponse) res;
+//
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, sessioncookie");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//
+//        chain.doFilter(req, res);
+//    }
 
-        HttpServletResponse response = (HttpServletResponse) res;
-
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, sessioncookie");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-
-        chain.doFilter(req, res);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
