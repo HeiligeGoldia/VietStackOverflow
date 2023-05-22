59240af2e217092642f4e591b6d969b2fd26dbfc
Duy <heiligegoldia@gmail.com>
2023-05-22 17:29:21 +0700

    fix config cors 8

--- src/main/java/com/se/kltn/vietstack/config/SimpleCORSFilter.java
+++ src/main/java/com/se/kltn/vietstack/config/SimpleCORSFilter.java
@@ -1,28 +1,33 @@
 package com.se.kltn.vietstack.config;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class SimpleCORSFilter extends GenericFilterBean {
+@Configuration
+public class SimpleCORSFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) res;
         httpResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         httpResponse.setHeader("Access-Control-Max-Age", "3600");
         httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
-        httpResponse.setHeader("Access-Control-Expose-Headers", "Authorization");
-        chain.doFilter(request, response);
+        chain.doFilter(req, res);
+
+    }
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
     }
 
 }
